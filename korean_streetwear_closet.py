162f53dacb089cf8b27fa883ac375e922aa79f86
annatrosmane <anna.trosmane17@gmail.com>
2026-04-14 23:25:16 +0300

    Create Korean streetwear database in python file
    Added a database of Korean streetwear items categorized into tops, bottoms, outerwear, shoes, bags, and accessories.

---
+++ korean_streetwear_closet.py
@@ -0,0 +1,53 @@
+KOREAN_STREETWEAR_DB = {
+  "tops": [
+    "korean oversized t shirt men",
+    "korean graphic oversized t shirt men",
+    "korean streetwear long sleeve tee men",
+    "korean oversized hoodie men",
+    "korean zip up hoodie men",
+    "korean striped long sleeve shirt men",
+    "korean streetwear knit sweater men",
+    "korean layered long sleeve shirt men",
+    "korean oversized sweatshirt men"
+  ],
+
+  "bottoms": [
+    "korean wide leg pants men",
+    "korean baggy jeans men",
+    "korean straight leg jeans men",
+    "korean streetwear cargo pants men",
+    "korean drawstring pants men",
+    "korean loose fit trousers men"
+  ],
+
+  "outerwear": [
+    "korean oversized bomber jacket men",
+    "korean streetwear varsity jacket men",
+    "korean oversized denim jacket men",
+    "korean windbreaker jacket men",
+    "korean streetwear puffer jacket men"
+  ],
+
+  "shoes": [
+    "korean chunky sneakers men",
+    "korean platform sneakers men",
+    "korean streetwear skate shoes men",
+    "korean casual sneakers men"
+  ],
+
+  "bags": [
+    "korean streetwear crossbody bag men",
+    "korean sling bag men",
+    "korean streetwear shoulder bag men",
+    "korean messenger bag men"
+  ],
+
+  "accessories": [
+    "korean beanie men",
+    "korean baseball cap men",
+    "korean streetwear chain necklace men",
+    "korean silver ring men",
+    "korean rectangle sunglasses men",
+    "korean style umbrella men"
+  ]
+}
