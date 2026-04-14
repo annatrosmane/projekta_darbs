915ca88d63bd5dafb1ddc909f27bc7c6a181ef8a
annatrosmane <anna.trosmane17@gmail.com>
2026-04-14 23:23:47 +0300

    Add Y2K fashion database with clothing items

---
+++ y2k_closet.py
@@ -0,0 +1,122 @@
+Y2K_DB = {
+  "tops": [
+    "y2k baby tee",
+    "y2k cropped vintage hoodie",
+    "y2k oversized vintage hoodie",
+    "y2k graphic baby tee",
+    "y2k rhinestone graphic tee",
+    "y2k ribbed tank top",
+    "y2k halter top",
+    "y2k lace trim cami top",
+    "y2k mesh long sleeve top",
+    "y2k sheer long sleeve top",
+    "y2k off shoulder top",
+    "y2k cropped cardigan",
+    "y2k bolero shrug",
+    "y2k velour tracksuit jacket"
+  ],
+
+  "bottoms": [
+    "y2k low rise baggy jeans",
+    "y2k baggy skate jeans",
+    "y2k wide leg jeans",
+    "y2k parachute pants",
+    "y2k track pants with stripes",
+    "y2k velour tracksuit pants",
+    "y2k flare leggings",
+    "y2k yoga pants flare",
+    "y2k low rise shorts"
+  ],
+
+  "skirts": [
+    "y2k denim mini skirt",
+    "y2k pleated mini skirt",
+    "y2k cargo mini skirt",
+    "y2k low rise mini skirt",
+    "y2k denim maxi skirt",
+    "y2k asymmetrical skirt",
+    "y2k layered mini skirt"
+  ],
+
+  "dresses": [
+    "y2k slip dress",
+    "y2k satin slip dress",
+    "y2k halter mini dress",
+    "y2k bodycon mini dress",
+    "y2k rhinestone mini dress",
+    "y2k velvet mini dress",
+    "y2k mesh mini dress",
+    "y2k lace trim mini dress",
+    "y2k cami mini dress"
+  ],
+
+  "outerwear": [
+    "y2k puffer jacket",
+    "y2k cropped puffer jacket",
+    "y2k oversized leather jacket",
+    "y2k faux fur jacket",
+    "y2k bomber jacket",
+    "y2k denim trucker jacket",
+    "y2k cropped zip hoodie",
+    "y2k velour tracksuit jacket",
+    "y2k winter jacket",
+    "j2k warm jacket"
+  ],
+
+  "shoes": [
+    "y2k chunky sneakers",
+    "y2k platform sneakers",
+    "y2k platform sandals",
+    "y2k chunky skate shoes",
+    "y2k combat boots",
+    "y2k knee high boots",
+    "adidas samba y2k outfit",
+    "new balance 530 y2k"
+  ],
+
+  "bags": [
+    "y2k shoulder bag",
+    "y2k baguette bag",
+    "y2k mini shoulder bag",
+    "y2k nylon shoulder bag",
+    "y2k metallic mini bag",
+    "y2k rhinestone shoulder bag"
+  ],
+
+  "belts": [
+    "y2k studded belt",
+    "y2k chain belt",
+    "y2k double buckle belt",
+    "y2k grommet belt"
+  ],
+
+  "headwear": [
+    "y2k trucker hat",
+    "y2k fuzzy bucket hat",
+    "y2k denim bucket hat",
+    "y2k bandana head scarf"
+  ],
+
+  "accessories": [
+    "y2k arm warmers",
+    "y2k fingerless gloves",
+    "y2k leg warmers",
+    "y2k fishnet tights",
+    "y2k tinted sunglasses",
+    "y2k narrow sunglasses",
+    "y2k butterfly hair clips",
+    "y2k claw hair clip",
+    "y2k rhinestone hair clips",
+    "y2k umbrella"
+  ],
+
+  "jewelry": [
+    "y2k butterfly necklace",
+    "y2k rhinestone choker",
+    "y2k nameplate necklace",
+    "y2k belly chain",
+    "y2k chunky hoop earrings",
+    "y2k beaded bracelet",
+    "y2k charm bracelet"
+  ]
+}
