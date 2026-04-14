28bed4ebdda6335b1cf2a3140ab0454ee505fee8
annatrosmane <anna.trosmane17@gmail.com>
2026-04-14 23:23:11 +0300

    Add CLASSIC_DB with clothing items categorized

---
+++ classic_closet.py
@@ -0,0 +1,110 @@
+CLASSIC_DB = {
+  "tops": [
+    "classic white button up shirt women",
+    "classic cotton button down shirt",
+    "old money striped button up shirt",
+    "classic slim fit blouse women",
+    "classic silk blouse women",
+    "old money knit polo sweater",
+    "classic ribbed knit sweater women",
+    "classic crew neck sweater women",
+    "classic v neck sweater women",
+    "old money cable knit sweater",
+    "classic turtleneck sweater women",
+    "classic fitted long sleeve top",
+    "classic polo shirt women"
+  ],
+
+  "bottoms": [
+    "classic tailored trousers women",
+    "old money high waist straight trousers",
+    "classic wide leg suit pants women",
+    "classic pleated trousers women",
+    "classic straight leg jeans women",
+    "classic dark wash straight jeans",
+    "classic beige chinos women",
+    "classic tailored ankle pants women"
+  ],
+
+  "skirts": [
+    "classic a line midi skirt",
+    "classic pleated midi skirt",
+    "old money wool midi skirt",
+    "classic straight midi skirt",
+    "classic high waist pencil skirt",
+    "classic beige midi skirt",
+    "classic satin midi skirt"
+  ],
+
+  "dresses": [
+    "classic midi shirt dress women",
+    "old money knit midi dress",
+    "classic wrap midi dress",
+    "classic long sleeve midi dress",
+    "classic sleeveless midi dress",
+    "old money elegant midi dress",
+    "classic minimalist black dress"
+  ],
+
+  "outerwear": [
+    "classic wool coat women",
+    "old money long wool coat",
+    "classic trench coat women",
+    "classic double breasted coat",
+    "classic tailored blazer women",
+    "old money oversized blazer",
+    "classic tweed blazer women",
+    "classic wool blend coat women"
+  ],
+
+  "shoes": [
+    "classic leather loafers women",
+    "old money penny loafers",
+    "classic ballet flats women",
+    "classic pointed toe heels",
+    "classic slingback heels women",
+    "classic ankle boots women"
+  ],
+
+  "bags": [
+    "classic leather tote bag women",
+    "old money structured handbag",
+    "classic minimalist shoulder bag",
+    "classic leather crossbody bag",
+    "classic top handle handbag",
+    "classic work tote bag women"
+  ],
+
+  "belts": [
+    "classic leather belt women",
+    "old money gold buckle belt",
+    "classic thin leather belt",
+    "classic brown leather belt"
+  ],
+
+  "headwear": [
+    "classic wool beret women",
+    "classic wool hat women",
+    "classic baseball cap women",
+    "classic wool scarf women"
+  ],
+
+  "accessories": [
+    "classic silk scarf women",
+    "old money square silk scarf",
+    "classic minimalist sunglasses",
+    "classic rectangle sunglasses",
+    "classic leather gloves women",
+    "classic wool gloves women",
+    "umbrella women"
+  ],
+
+  "jewelry": [
+    "classic pearl necklace women",
+    "classic pearl stud earrings",
+    "old money gold hoop earrings",
+    "classic gold chain necklace",
+    "classic minimalist gold bracelet",
+    "classic elegant ring women"
+  ]
+}
