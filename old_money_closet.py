f90552510277b1e0d25e34cca74178cc7380a433
annatrosmane <anna.trosmane17@gmail.com>
2026-04-14 23:26:02 +0300

    Add OLD_MONEY_DB with clothing and accessories

---
+++ old_money_closet.py
@@ -0,0 +1,54 @@
+OLD_MONEY_DB = {
+  "tops": [
+    "mens polo shirt classic",
+    "mens linen button up shirt",
+    "mens oxford shirt cotton",
+    "mens cable knit sweater",
+    "mens v neck sweater",
+    "mens turtleneck sweater",
+    "mens striped button up shirt"
+  ],
+
+  "bottoms": [
+    "mens tailored trousers",
+    "mens pleated trousers",
+    "mens wool trousers men",
+    "mens straight leg chinos",
+    "mens classic straight jeans"
+  ],
+
+  "outerwear": [
+    "mens wool overcoat",
+    "mens trench coat men",
+    "mens classic blazer",
+    "mens tweed blazer men",
+    "mens wool coat men"
+  ],
+
+  "shoes": [
+    "mens leather loafers",
+    "mens penny loafers",
+    "mens derby shoes men",
+    "mens leather oxford shoes",
+    "mens suede loafers"
+  ],
+
+  "bags": [
+    "mens leather briefcase",
+    "mens leather messenger bag",
+    "mens leather tote bag"
+  ],
+
+  "accessories": [
+    "mens leather belt classic",
+    "mens silk scarf men",
+    "mens classic sunglasses",
+    "mens leather gloves"
+  ],
+
+  "jewelry": [
+    "mens gold signet ring",
+    "mens minimalist ring",
+    "mens classic watch"
+  ]
+}
