c2999d4221bfcfe03ab26c6d6563edde1f0da782
annatrosmane <anna.trosmane17@gmail.com>
2026-04-14 23:22:22 +0300

    Initialize ACUBI_DB with clothing and accessories
    Added ACUBI_DB dictionary containing various clothing and accessory items categorized by type.

---
+++ acubi_closet.py
@@ -0,0 +1,111 @@
+ACUBI_DB = {
+  "tops": [
+    "acubi basic cropped t-shirt",
+    "acubi slim fit long sleeve top",
+    "acubi ribbed knit long sleeve",
+    "acubi asymmetrical long sleeve top",
+    "acubi off shoulder knit top",
+    "acubi cropped cardigan korean style",
+    "acubi button up cardigan slim",
+    "acubi soft knit sweater women",
+    "acubi square neck long sleeve top",
+    "acubi layered look long sleeve top",
+    "acubi basic fitted turtleneck",
+    "acubi cropped zip hoodie",
+    "acubi minimalist baby tee"
+  ],
+
+  "bottoms": [
+    "acubi low rise straight jeans women",
+    "acubi baggy straight leg jeans women",
+    "acubi washed vintage jeans women",
+    "acubi korean wide leg pants women",
+    "acubi loose fit cargo pants women",
+    "acubi drawstring straight pants women",
+    "acubi casual straight sweatpants",
+    "acubi wide leg suit pants women",
+    "acubi low waist baggy trousers women"
+  ],
+
+  "skirts": [
+    "acubi low rise mini skirt",
+    "acubi pleated mini skirt korean",
+    "acubi basic a line mini skirt",
+    "acubi midi knit skirt",
+    "acubi low waist straight skirt",
+    "acubi minimalist maxi skirt",
+    "acubi cargo mini skirt"
+  ],
+
+  "dresses": [
+    "acubi minimalist slip dress",
+    "acubi ribbed knit midi dress",
+    "acubi long sleeve bodycon dress",
+    "acubi square neck midi dress",
+    "acubi basic tank midi dress",
+    "acubi korean minimalist mini dress"
+  ],
+
+  "outerwear": [
+    "acubi winter padded jacket",
+    "acubi wool long coat",
+    "acubi insulated minimalist coat",
+    "acubi vintage heavy jacket",
+    "acubi lightweight windbreaker (for warm weather only)"
+  ],
+
+  "shoes": [
+    "acubi chunky sneakers women",
+    "acubi korean platform sneakers women",
+    "acubi minimalist sneakers women",
+    "acubi black chunky loafers women",
+    "acubi square toe ankle boots women",
+    "acubi korean platform boots women",
+    "acubi basic ballet flats women"
+  ],
+
+  "bags": [
+    "acubi minimalist shoulder bag",
+    "acubi korean nylon shoulder bag",
+    "acubi soft leather shoulder bag",
+    "acubi small hobo bag women",
+    "acubi minimalist tote bag",
+    "acubi vintage shoulder bag"
+  ],
+
+  "belts": [
+    "acubi minimalist leather belt",
+    "acubi thin black belt women",
+    "acubi vintage buckle belt",
+    "acubi simple silver buckle belt"
+  ],
+
+  "headwear": [
+    "acubi baseball cap korean",
+    "acubi minimalist beanie",
+    "acubi knitted beanie women",
+    "acubi vintage washed cap"
+  ],
+
+  "accessories": [
+    "acubi arm warmers knit",
+    "acubi leg warmers korean",
+    "acubi minimalist sunglasses",
+    "acubi narrow rectangle sunglasses",
+    "acubi basic hair claw clip",
+    "acubi minimalist hair clip",
+    "acubi sheer tights",
+    "acubi knit scarf women",
+    "cute umbrella"
+  ],
+
+  "jewelry": [
+    "acubi minimalist silver necklace",
+    "acubi thin chain necklace",
+    "acubi korean layered necklace",
+    "acubi small hoop earrings",
+    "acubi minimalist ring set",
+    "acubi simple chain bracelet",
+    "acubi korean silver jewelry set"
+  ]
+}
